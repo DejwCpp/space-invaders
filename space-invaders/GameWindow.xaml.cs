dcfdd84f88e07c4f1a57e963b25cb42f3c98bda1
Wojciech Szymański <61559054+wojciechszymanski-dev@users.noreply.github.com>
2025-03-28 08:45:29 +0100

    redo repo

---
+++ space-invaders/GameWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Space_intruders
+{
+    public partial class GameWindow : Window
+    {
+        int marginPoz = 185;
+        Player player = new();
+        static int counter = 0;
+        public Dictionary<int, Arrow> arrows = new Dictionary<int, Arrow>();
+        public List<Shield> shields = new List<Shield>();
+        public Enemies enemies { get; private set; }
+        bool canShoot = true;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            this.PreviewKeyDown += new KeyEventHandler(PlayerMovement);
+            InitializePlayer();
+            enemies = new Enemies(gameCanvas);
+            enemies.InitializeEnemies();
+            InitializeShields();
+        }
+
+        private void InitializePlayer()
+        {
+            player.SetID(0);
+            player.SetHP(3);
+            player.SetSpeed(20);
+            player.SetArmour(1);
+            player.SetDMG(1);
+        }
+        private void InitializeShields()
+        {
+            double[] shieldPositions = { 50, 200, 350 };
+
+            foreach (double posX in shieldPositions)
+            {
+                shields.Add(new Shield(gameCanvas, posX, 400));
+            }
+        }
+        public void SpawnNewArrow()
+        {
+            try
+            {
+                Arrow arrow = new Arrow();
+                Interlocked.Increment(ref counter);
+                arrow.SetID(counter);
+                arrow.SetDMG(player.GetDMG());
+                arrow.SetSpeed(10);
+                arrow.SetGameWindow(this);
+
+                // Create the arrow image
+                BitmapImage imageSource = new BitmapImage(new Uri("/Resources/arrow.png", UriKind.Relative));
+
+                Image arrowImage = new Image()
+                {
+                    Source = imageSource,
+                    Width = 30,
+                    Height = 30,
+                };
+
+                // Position the arrow at the player's position
+                double playerTop = Canvas.GetTop(playerImage);
+                Canvas.SetLeft(arrowImage, Canvas.GetLeft(playerImage) + playerImage.Width / 2 - arrowImage.Width / 2);
+                Canvas.SetTop(arrowImage, playerTop - arrowImage.Height);
+
+                Debug.WriteLine($"Arrow created at position: Left={Canvas.GetLeft(arrowImage)}, Top={Canvas.GetTop(arrowImage)}");
+
+                gameCanvas.Children.Add(arrowImage);
+                arrow.SetImageSource(arrowImage);
+
+                arrows.Add(arrow.GetID(), arrow);
+
+                arrow.SetTimer();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating arrow: " + ex.Message);
+            }
+        }
+
+        public void RemoveArrow(int arrowID)
+        {
+            if (arrows.TryGetValue(arrowID, out Arrow arrow))
+            {
+                Dispatcher.Invoke(() => {
+                    gameCanvas.Children.Remove(arrow.GetImageSource());
+                    arrows.Remove(arrowID);
+                });
+            }
+        }
+
+        public async void PlayerMovement(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Right:
+                case Key.D:
+                    marginPoz += player.GetSpeed();
+                    Canvas.SetLeft(playerImage, marginPoz);
+                    break;
+                case Key.Left:
+                case Key.A:
+                    marginPoz -= player.GetSpeed();
+                    Canvas.SetLeft(playerImage, marginPoz);
+                    break;
+                case Key.Space:
+                    if (canShoot)
+                    {
+                        canShoot = false;
+                        SpawnNewArrow();
+                        await Task.Delay(500);
+                        canShoot = true;
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ space-invaders/Shield.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+public class Shield
+{
+    private Canvas canvas;
+    private Image shieldImage;
+    private int durability;
+
+    private string[] shieldImages =
+    {
+        "/Resources/shield3.png",
+        "/Resources/shield2.png",
+        "/Resources/shield.png"
+    };
+
+    public Shield(Canvas gameCanvas, double posX, double posY)
+    {
+        canvas = gameCanvas;
+        durability = 3;
+
+        shieldImage = new Image()
+        {
+            Width = 70,
+            Height = 53,
+            Source = new BitmapImage(new Uri(shieldImages[0], UriKind.Relative))
+        };
+
+        Canvas.SetLeft(shieldImage, posX - shieldImage.Width / 2);
+        Canvas.SetTop(shieldImage, posY);
+        canvas.Children.Add(shieldImage);
+    }
+
+    public Image GetImage() => shieldImage;
+    public int GetDurability() => durability;
+
+    public void TakeDamage()
+    {
+        durability--;
+
+        if (durability > 0)
+        {
+            shieldImage.Source = new BitmapImage(new Uri(shieldImages[3 - durability], UriKind.Relative));
+        }
+        else
+        {
+            canvas.Children.Remove(shieldImage);
+        }
+    }
+}
